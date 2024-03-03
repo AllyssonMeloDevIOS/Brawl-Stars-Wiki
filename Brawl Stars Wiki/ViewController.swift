6552a83234996ba8bb9888b8175ae069d79be4b4
Allysson Melo <allymelox@gmail.com>
2024-03-03 13:38:54 -0300

    Initial Commit

---
+++ Brawl Stars Wiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Brawl Stars Wiki
+//
+//  Created by admin on 03/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
